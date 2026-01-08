d4a1bcfcb94b553dfebbe64da1a0ec41495f521a
mdsddm <md.saddam.gtm@gmail.com>
2026-01-09 00:28:21 +0530

    added views

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+
 const MONGO_URL = 'mongodb://127.0.0.1:27017/staynest';
 main().then(() => {
     console.log('Connected to DB');
@@ -14,6 +17,9 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // basic route
 app.get("/", (req, res) => {
@@ -22,8 +28,26 @@ app.get("/", (req, res) => {
 
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("index.ejs", { allListings });
+    res.render("listings/index.ejs", { allListings });
 });
+
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+app.get("/listings/new", async (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+
 
 
 app.listen(8080, () => {
