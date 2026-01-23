f1abd254cacf96dc33f17bdc6ef013c0b66dd9c8
mdsddm <md.saddam.gtm@gmail.com>
2026-01-23 20:38:08 +0530

    added map

--- public/js/map.js
+++ public/js/map.js
@@ -6,14 +6,12 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap contributors",
 }).addTo(map);
 
-// initial marker with custom message
 let marker = L.marker(coordinates)
   .addTo(map)
   .bindPopup(
     `<strong>${placeName}</strong><br>Exact location will be provided after booking`,
   );
 
-// on click, move marker and show coords popup
 map.on("click", ({ latlng: { lat, lng } }) => {
   marker
     .setLatLng([lat, lng])
