4aa3366f037fb37083e5c8922fddefaa3fcf70e3
mdsddm <md.saddam.gtm@gmail.com>
2026-01-24 21:14:10 +0530

    completed

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const sessionOptions = {
   secret: "mysupersecretcode",
   resave: false,
   saveUninitialized: true,
-  Cookie: {
+  cookie: {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
